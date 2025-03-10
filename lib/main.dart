b729732053a1cbe2e3aa27f9f23ce1d0956322af
KoBrAIbrahim <111666217+KoBrAIbrahim@users.noreply.github.com>
2025-03-10 13:53:38 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:main_app/contraller/theme_notifier.dart';
+import 'package:main_app/login_page.dart';
+import 'package:main_app/home_page.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(),
+      child: MyApp(),
+    ),
+  );
+}
+
+final GoRouter _router = GoRouter(
+  initialLocation: '/login',
+  routes: [
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => LoginPage(),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) {
+        final username = state.extra as String;
+        return HomePage(username: username);
+      },
+    ),
+  ],
+);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      themeMode: themeNotifier.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.indigo,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.indigo,
+      ),
+      routerConfig: _router, 
+    );
+  }
+}
